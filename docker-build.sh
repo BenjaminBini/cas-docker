1d71225bb56e1855ba25e2011fce4c42b20f62f4
Misagh Moayyed <mm1844@gmail.com>
2019-08-20 11:49:36 +0430

    updated

--- docker-build.sh
+++ docker-build.sh
@@ -3,7 +3,7 @@
 image_tag=(`cat gradle.properties | grep "cas.version" | cut -d= -f2`)
 
 echo "Building CAS docker image tagged as [$image_tag]"
-read -p "Press [Enter] to continue..." any_key;
+# read -p "Press [Enter] to continue..." any_key;
 
 docker build --tag="org.apereo.cas/cas:$image_tag" . \
   && echo "Built CAS image successfully tagged as org.apereo.cas/cas:$image_tag" \
